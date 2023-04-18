3dda46b98c1d3ec27363dae071f57acfb2d04612
Sireesha Kudupuri <21A91A05F2@aec.edu.in>
2023-04-18 19:07:55 +0530

    Commited first git Example repository

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Takeinput()
+{
+       #ifndef ONLINE_JUDGE
+       freopen("input.txt","r",stdin);
+       freopen("output.txt","w",stdout);
+       #endif
+}
+
+
+int main(){
+	Takeinput();
+	cout<<" Hello gitHub ";
+}
